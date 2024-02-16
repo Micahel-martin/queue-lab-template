9bced2bcf99bbd1effa3e5bb1145648eb5033ac4
Micahel-martin <michael.martin@students.fhu.edu>
2024-02-15 18:54:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Queue():
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def pop(self):
-        self.cards[0]
+        del self.cards[0]
 
 if __name__ == '__main__':
     #REMOVE PASS AND YOUR CODE GOES HERE
